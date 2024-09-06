fed3a40b2f2eadf8ec56e6f7137db2377c66afa7
SP4RKiOP <68564589+SP4RKiOP@users.noreply.github.com>
2024-09-06 17:24:09 +0530

    h-[calc(100dvh)]

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,13 +2,11 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/auth";
 import Provider from "./client-provider";
 import "./globals.css";
-import Chat from "@/components/chat";
 import { Toaster } from "@/components/ui/toaster";
 import { Inter } from "next/font/google"
-import { cn } from "@/lib/utils"
 
 export const metadata = {
-  title: "Eva.AI",
+  title: "Eva",
   description: "Made by Abhishek Sinha",
   content:"width=device-width, initial-scale=1.0"
 };
@@ -26,7 +24,7 @@ export default async function RootLayout({children,
     <html lang="en">
       <body className={inter.className}>
         <Provider session={session}>
-          <main className="h-svh">{children}</main>
+          <main className="h-[calc(100dvh)]">{children}</main>
           <Toaster />
           </Provider>
       </body>
--- frontend/components/greet.tsx
+++ frontend/components/greet.tsx
@@ -5,11 +5,11 @@ const Greet: React.FC = () => {
     return (
         <div className="flex flex-col items-center mb-[calc(100vh-60vh)]">
             <div className="relative">
-                <div className="mb-3 h-16 w-16">
+                <div className="h-16 w-16">
                     <img src="/icon.svg" alt="Eva" />
                 </div>
             </div>
-            <div className="mb-5 text-4xl font-medium">Eva.AI</div>
+            <div className="pt-6 text-4xl font-medium">Eva the Assistant</div>
         </div>
     );
 }
